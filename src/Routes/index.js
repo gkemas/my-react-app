4349e5803ce6e6a64bfd261a6194afa3a96ececf
gkemas <kemasgokhan@gmail.com>
2024-01-07 22:26:22 +0300

    route

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { authProtectedRoutes, publicRoutes } from "./AllRoutes";
 const Index = () =>{
+  const availablePublicRoutesPaths = publicRoutes.map((r) => r.path);
+  const availableAuthRoutesPath =authProtectedRoutes.map((r)=>r.path);
 return (
     <Switch>
-      <Route exact path="/" component={Home} />
+      {/* <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact}/>
+      <Route path="/contact" component={Contact}/> */}
       {/* Diğer rotaları burada tanımlayabilirsiniz */}
+      <Route path={availablePublicRoutesPaths}>
+                    <NonAuthLayout>
+                        <Switch>
+                            {publicRoutes.map((route, idx) => (
+                                <Route
+                                    path={route.path}
+                                    component={route.component}
+                                    key={idx}
+                                    exact={true}
+                                />
+                            ))}
+                        </Switch>
+                    </NonAuthLayout>
+                </Route>
 
     </Switch>
 );
